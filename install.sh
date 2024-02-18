6ff1500cd2d1c60747f6250910d1e00ae6c6e557
alonsogonzalezleal <alonsogonzalez423@gmail.com>
2024-02-18 03:54:54 -0600

    backup

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ install_script() {
     echo "Installing xfwall in /usr/local/bin/..."
     sudo cp "$0" /usr/local/bin/xfwall
     sudo chmod +x /usr/local/bin/xfwall
+    mkdir ~/.xfwall/history
     echo "Installation completed! Try running xfwall"
 }
 
--- xfwall.sh
+++ xfwall.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
-# Función para obtener la URL del fondo de pantalla desde la API de Wallhaven
+# Get URL from wallpaper
 get_wallpaper_url() {
-    # Realizar la solicitud a la API de Wallhaven y extraer la URL del fondo de pantalla
-    # wallpaper_url=$(curl -s "https://wallhaven.cc/api/v1/w/l8v3ey" | jq -r '.data[0].path')
-    wallpaper_url=$(curl -s "https://wallhaven.cc/api/v1/w/l8v3ey")
+
+
+    random_index=$((RANDOM % 31))
+    tag="landscape"
+    categories="110"
+    purity="100"
+    apikey="2RD4RdmlgWyjmUbInbkwL2PMuJQnscwO"
+
+
+    wallpaper_url=$(curl -s "https://wallhaven.cc/api/v1/search?q=$tag&categories=$categories&purity=$purity&apikey=$apikey" | jq -r ".data[$random_index].path")
     echo "$wallpaper_url"
+}
+
+# Download wallpaper
+download_wallpaper() {
+    local wallpaper_url="$1"
+    curl -o ~/.xfwall/history/last_img.jpg "$wallpaper_url"
 }
 
 # Función para establecer el fondo de pantalla en XFCE4
 set_wallpaper() {
-    # Comando para cambiar el fondo de pantalla en XFCE4, ajusta la ruta según tu configuración
-    # xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -s "$1"
-    xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitoreDP1/workspace0 -s "$1"
+    xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitorDP1/workspace0/last-image --set ~/.xfwall/history/last_img.jpg
 }
 
-# Establecer el tiempo en segundos para cambiar el fondo de pantalla
-interval=1800  # Cambia cada 30 minutos, ajusta según tus preferencias
+# Time measured in seconds
+interval=10
 
 while true; do
     wallpaper_url=$(get_wallpaper_url)
+    download_wallpaper "$wallpaper_url"
     set_wallpaper "$wallpaper_url"
     sleep "$interval"
 done
