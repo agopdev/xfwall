93389c530a15edaa8d174e9bc27fe3da595d8046
alonsogonzalezleal <alonsogonzalez423@gmail.com>
2024-02-18 13:26:22 -0600

    Backup

--- xfwall.sh
+++ xfwall.sh
@@ -1,37 +1,61 @@
 #!/bin/bash
 
-# Get URL from wallpaper
+# Check that required dependencies are installed
+check_dependencies() {
+    local dependencies=("jq" "curl")
+
+    for dep in "${dependencies[@]}"; do
+        if ! command -v "$dep" &> /dev/null; then
+            echo "Error: '$dep' is not installed. Please install before executing xfwall."
+            exit 1
+        fi
+    done
+}
+
+check_dependencies
+
+# Get wallpaper URL and select one randomly
 get_wallpaper_url() {
+    local tag="landscapes"
+    local categories="100"
+    local purity="100"
+    local apikey=""
 
+    local output_json="/tmp/output.json"
 
-    random_index=$((RANDOM % 31))
-    tag="landscape"
-    categories="110"
-    purity="100"
-    apikey=""
+    curl -s "https://wallhaven.cc/api/v1/search?q=$tag&categories=$categories&purity=$purity&apikey=$apikey" > "$output_json"
 
+    local random_index
+    total_elements=$(jq '.data | length' "$output_json")
+    random_index=$(shuf -i 0-$((total_elements - 1)) -n 1)
 
-    wallpaper_url=$(curl -s "https://wallhaven.cc/api/v1/search?q=$tag&categories=$categories&purity=$purity&apikey=$apikey" | jq -r ".data[$random_index].path")
+    local wallpaper_url
+    wallpaper_url=$(jq -r ".data[$((random_index))].path" "$output_json")
+
+    rm "$output_json"
+
     echo "$wallpaper_url"
 }
 
-# Download wallpaper
-download_wallpaper() {
+# Download and set wallpaper
+update_wallpaper() {
     local wallpaper_url="$1"
-    curl -o ~/.xfwall/history/last_img.jpg "$wallpaper_url"
+    local image_path=~/.xfwall/history/last_img.jpg
+
+    curl -o "$image_path" "$wallpaper_url"
+
+    if [ $? -eq 0 ]; then
+        xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitoreDP1/workspace0/last-image --set "$image_path"
+    else
+        echo "Error: The wallpaper could not be downloaded."
+    fi
 }
 
-# Función para establecer el fondo de pantalla en XFCE4
-set_wallpaper() {
-    xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitorDP1/workspace0/last-image --set ~/.xfwall/history/last_img.jpg
-}
-
-# Time measured in seconds
+# Time in seconds
 interval=10
 
 while true; do
     wallpaper_url=$(get_wallpaper_url)
-    download_wallpaper "$wallpaper_url"
-    set_wallpaper "$wallpaper_url"
+    update_wallpaper "$wallpaper_url"
     sleep "$interval"
 done
